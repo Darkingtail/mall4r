060eb57fddff7862de3e65ae07d8e36dc60bb42d
Darkingtail <1603774836@qq.com>
2025-12-02 21:49:55 +0800

    feat: add prod-list api

---
+++ src/service/api/prod/prod-list/index.ts
@@ -0,0 +1,45 @@
+import { type HttpClient, httpClient } from "@/service";
+
+export interface ProdListItem {
+    prodName: string;
+}
+
+export type ProdList = ProdListItem[]
+
+export interface FetchProdListPageRequestPayload {
+    t: number;
+}
+
+export interface FetchProdListPageResponse {
+    current: number;
+    pages: number;
+    records: ProdList;
+    size: number;
+    total: number;
+}
+
+const PROD_LIST_BASE_PATH = "/prod/prod";
+const PAGE = `${PROD_LIST_BASE_PATH}/page`;
+
+export function createUserApi(client: HttpClient = httpClient) {
+    return {
+        fetchProdListPage<T = FetchProdListPageResponse>(
+                    params: FetchProdListPageRequestPayload,
+                ): Promise<T> {
+			const urlParams = Object.entries(params)
+				.map(([key, value]) => {
+					if (value === undefined || value === null || value === "") {
+						return "";
+					}
+					return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+				})
+				.filter(Boolean)
+				.join("&");
+
+                const url = urlParams ? `${PAGE}?${urlParams}` : PAGE;
+                    return client.get<T, T>(url);
+                },
+    };
+}
+
+export const userApi = createUserApi();
